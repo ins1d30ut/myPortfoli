02b106236ad2b6186ed7c2ab0b321beef4fb2ec7
ins1d30ut <nfteebsv@protonmail.com>
2022-05-17 23:45:53 -0500

    commit event item

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
+import { getFeaturedEvents } from '../dummy-data';
+
 function HomePage() {
-  return (
-    <div>
-      <h1>The Home Page</h1>
-    </div>
-  );
+  const featuredEvents = getFeaturedEvents();
+
+  return <div></div>;
 }
 
 export default HomePage;
